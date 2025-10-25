635449bea2c133423393b8ff6795726de6550e99
NathanL05 <nathanl1605@gmail.com>
2025-10-25 17:40:42 +0100

    [FE-14] Contact Page UI

--- frontend/src/features/contact/components/StoreInfoWireframe.jsx
+++
@@ -1,9 +0,0 @@
-export default function StoreInfoWireframe() {
-  return (
-    <div className="space-y-6">
-      <div className="h-40 rounded-xl border border-dashed border-stone-300" />
-      <div className="h-40 rounded-xl border border-dashed border-stone-300" />
-    </div>
-  );
-}
-
